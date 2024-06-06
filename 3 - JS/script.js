927e012e29746e69171ab4f23ac8b0aabb1d38ef
KatShunpos <aoun.maxime@outlook.fr>
2024-06-06 15:58:44 +0200

    ajout de fichier

--- 3 - JS/script.js
+++ 3 - JS/script.js
@@ -1,13 +1,12 @@
-let input;
-
-if (input == "tshirt") {
-  console.log("T-Shirt");
-} else if (input == "polo") {
-  console.log("Polo");
-} else if (input == "veste") {
-  console.log("Veste");
-} else if (input == "short") {
-  console.log("Short");
-} else {
-  console.log("Unknown item");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const forms = document.querySelectorAll("form");
+  forms.forEach((form) => {
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Empêche le formulaire de se soumettre de manière normale
+      const button = form.querySelector('button[type="submit"]');
+      const url = button.getAttribute("data-url");
+      console.log("Redirection vers : ", url);
+      window.location.href = url; // Redirige vers la nouvelle URL
+    });
+  });
+});
